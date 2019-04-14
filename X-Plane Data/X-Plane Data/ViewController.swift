967c4a217b89ac09318529e5b65ac00f906b9617
Hangyu Ye <jerry.hangyu@gmail.com>
2019-04-14 14:36:59 +0800

    Project created

---
+++ X-Plane Data/X-Plane Data/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  X-Plane Data
+//
+//  Created by Jerry on 4/14/19.
+//  Copyright © 2019 SoftChain Foundation Ltd. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import CoreMotion
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
